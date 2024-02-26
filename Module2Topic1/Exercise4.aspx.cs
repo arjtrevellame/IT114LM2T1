97c42d882fdf6f36ee89a4568f98800de7b97e69
arjtrevellame <154643699+arjtrevellame@users.noreply.github.com>
2024-02-26 22:42:16 +0800

    Update Exercise4.aspx.cs

--- Module2Topic1/Exercise4.aspx.cs
+++ Module2Topic1/Exercise4.aspx.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,6 +13,18 @@ namespace Module1Exercise1
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            
+            string FullName= fullName.Text;
+            var Age = age.Text;
+            string eMail = email.Text;
+            string ConfirmEmail = confirmEmail.Text;
+            string Result = $"Full name: {FullName} |Age: {Age} |Email: {eMail} |Confirm Email: {ConfirmEmail}";
+            result.Text = Result;
+            
+            Response.Write($"<script>alert('Successfully Registered\\n\\nFull name: {FullName}\\nAge: {Age}\\nEmail: {eMail}\\nConfirm Email: {ConfirmEmail}')</script>");
         }
     }
 }
